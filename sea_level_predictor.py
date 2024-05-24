34117cd694a6ec7eb0b20e6c53bb3cf744294659
Emily Liu <41386372+milksencha@users.noreply.github.com>
2024-05-23 21:50:17 -0700

    Update sea_level_predictor.py

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
+import numpy as np
 
 def draw_plot():
     # Read data from file
@@ -17,16 +18,20 @@ def draw_plot():
     plt.scatter(year, sea_level, c = 'lightsteelblue')
 
     # Create first line of best fit
-    slope, yint, r, p, stderr = linregress(year, sea_level)
-    plt.plot(year, yint + slope * year, c = 'cornflowerblue')
+    fit = linregress(year, sea_level)
+
+    plt.plot(year, fit.intercept + fit.slope * year, c = 'cornflowerblue')
 
     # Create second line of best fit
-    slope_2000, yint_2000, r, p, stderr = linregress(year_2000, sea_level_2000)
-    plt.plot(year_2000, yint_2000 + slope_2000 * year_2000, c = 'orangered')
+    fit_2000 = linregress(year_2000, sea_level_2000)
+
+    plt.plot(year_2000, fit_2000.intercept + fit_2000.slope * year_2000, c = 'orangered')
 
     # Add labels and title
     ax.set_xlabel('Year')
     ax.set_ylabel('Sea Level (inches)')
+
+    plt.xticks(np.arange(1850.0, 2075.0 + 1.0, 25.0))
     
     ax.set_title('Rise in Sea Level')
     
